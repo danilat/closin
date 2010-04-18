a3360ee3fba64a50823b6646dcc895fc9f46d64d
Daniel Latorre Velilla <danilat@vodkalimon.local>
2010-04-18 22:09:12 +0200

    now we look the address using the lat and lon

--- closin/main.py
+++ closin/main.py
@@ -67,6 +67,12 @@ class BaseHandler(webapp.RequestHandler):
 				"lon": data['WFSResponse']['posXList'][i],
 				"id": parse_id(data['WFSResponse']['urlList'][i])})
 		self.create_service(name, result)
+	def getaddress(self, lat, lon):
+		response = urlfetch.fetch('http://maps.google.com/maps/geo?q='+lat+','+lon).content
+		data = json.loads(response)
+		#return data['Placemark']['address']
+		self.response.out.write(response)
+		#self.response.out.write(data['Placemark']['address'])
 
 class MainPage(BaseHandler):
 	def get(self):
@@ -124,10 +130,12 @@ class FecthBizi(BaseHandler):
 				
 		self.create_service("bizi", result)
   
-class Details(webapp.RequestHandler):
+class Details(BaseHandler):
 	def get(self):
 		service = self.request.get('service')
 		id = self.request.get('id')
+		lat = self.request.get('lat')
+		lon = self.request.get('lon')
 		response = ""
 		if service =="bus":
 			response = urlfetch.fetch('http://www.tuzsa.es/tuzsa_frm_esquemaparadatime.php?poste='+id).content
@@ -140,11 +148,15 @@ class Details(webapp.RequestHandler):
 				if not items.has_key(linenumber):
 					items[linenumber] = {'name': row.contents[1].string, 'buses':[]}
 				items[linenumber]['buses'].append(row.contents[2].string)
+			address = ''#self.getaddress(lat, lon)
 			template_values = {
 				'post' : id,
-				'address': 'C\ Rue del percebe 1',
-				'lines' : items
+				'address': address,
+				'lines' : items,
+				'lat': lat,
+				'lon':lon
 			}
+			
 			path = os.path.join(os.path.dirname(__file__), 'templates/bus.html')
 			self.response.out.write(template.render(path, template_values))
 		elif service =="bizi":
